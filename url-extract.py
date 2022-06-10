e992ce217343675d71be614ad8071314774198fd
Olympia Thornton <olympiat@azerox.org>
2022-06-09 20:01:44 -0500

    remove variables and add filetypes
    Remove some variables where they are not needed. Add filetypes to the
    askopenfilename call -> default is text files

--- url-extract.py
+++ url-extract.py
@@ -3,22 +3,19 @@ import re
 from tkinter.filedialog import askopenfilename
 
 # Open an askfile window
-file = askopenfilename()
+file = askopenfilename(
+    filetypes=[('Text Files', '*.txt'), ('All Files', '*.*')])
 
 # Set pattern for searching
 PATTERN = '\(([-a-z0-9@:%_\+.~#?&/=]*)?\.?([-a-z]*\.[-a-z]{2,4})(\.[-a-z]{2,4})?(\/[-a-zA-Z0-9@:%_\+.~#?&/=]*)?\)'
 
-# Open file and read data
-with open(file, 'r', encoding='utf-8') as text:
-    text = text.read()
-
-# Find matches in the data
-allMatches = re.findall(PATTERN, text)
+# Open file, read data, and find all matches
+with open(file, 'r', encoding='utf-8') as f:
+    allMatches = re.findall(PATTERN, f.read())
 
 # Print out the matches and the respective index
 for i, match in enumerate(allMatches):
     if len(match) > 1:
-        buildMatchString = ''.join(match)
-        print(f'{i} {buildMatchString}\n')
+        print(f'{i} {"".join(match)}\n')
     else:
         print(f'{i} {match}\n')
