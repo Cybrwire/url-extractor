6be3a721e228aa46c0fd38eee7014507ab819f7a
Olympia Thornton <olympiat@azerox.org>
2022-06-09 19:18:24 -0500

    syntax and reusability

--- url-extract.py
+++ url-extract.py
@@ -1,21 +1,24 @@
 
 import re
+from tkinter.filedialog import askopenfilename
 
+# Open an askfile window
+file = askopenfilename()
 
-with open('./properties-to-delete-in-gnl.txt') as text:
+# Set pattern for searching
+PATTERN = '\(([-a-z0-9@:%_\+.~#?&/=]*)?\.?([-a-z]*\.[-a-z]{2,4})(\.[-a-z]{2,4})?(\/[-a-zA-Z0-9@:%_\+.~#?&/=]*)?\)'
+
+# Open file and read data
+with open(file, 'r', encoding='utf-8') as text:
     text = text.read()
-    pattern = '\(([-a-z0-9@:%_\+.~#?&/=]*)?\.?([-a-z]*\.[-a-z]{2,4})(\.[-a-z]{2,4})?(\/[-a-zA-Z0-9@:%_\+.~#?&/=]*)?\)'
-    allMatches = re.findall(pattern, text)
 
-i = 1
+# Find matches in the data
+allMatches = re.findall(PATTERN, text)
 
-for match in allMatches:
+# Print out the matches and the respective index
+for match, i in enumerate(allMatches):
     if len(match) > 1:
         buildMatchString = ''.join(match)
-        print(str(i) + " " + buildMatchString)
-        print()
-        
+        print(f'{i} {buildMatchString}\n')
     else:
-        print(str(i) +  " " + match)
-        print()
-    i+=1
+        print(f'{i} {match}\n')
